0cbc18ab4cc1a0178dadff31568c797d76e085e2
anderson andrade <anderson@uniquemarketing.com.br>
2025-07-21 23:52:20 -0300

    Base de dados com TypeOrmModule

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  doLogin() {
-    return 'Olá do AuthService';
+  doLogin(loginDto: LoginDto) {
+    console.log(loginDto instanceof LoginDto, loginDto);
+    return loginDto;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get(':id')
+  findOne(@Param('id', CustomParseIntPipe) id: number) {
+    console.log(id, typeof id);
+    return `Olá do controller do user #${id}`;
+  }
+}
