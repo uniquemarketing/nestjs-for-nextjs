1d54478e0e998d4f9f715237fda37e58b9edd2ce
anderson andrade <anderson@uniquemarketing.com.br>
2025-07-24 19:32:02 -0300

    Protegendo rotas com JwtAuthGuard personalizado

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from './user.service';
-import { AuthGuard } from '@nestjs/passport';
+//import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
+//import { Request } from 'express';
+import { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 
 @Controller('user')
 export class UserController {
@@ -12,9 +15,10 @@ export class UserController {
     private readonly userService: UserService,
   ) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id', CustomParseIntPipe) id: number) {
+  findOne(@Req() req: AuthenticatedRequest, @Param('id', CustomParseIntPipe) id: number) {
+    console.log(req.user.id);
     return `Olá do controller do user #${id}`;
   }
 
